b4261c3277549a96d12cde7178d552983cd98e1e
Carlos Goyeneche <karlos.goye@gmail.com>
2015-06-05 16:40:45 -0500

    dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'usuario/index'
-
-  get 'miprueba/index'
-  get 'miprueba/edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,6 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get ':controller(/:action(/:id(.:format)))'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
